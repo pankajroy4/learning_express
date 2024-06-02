8ddf596037b38a4d2bb20a623e95b679b1f439b4
pankajroy4 <pankajroy842002@gmail.com>
2024-06-03 00:24:37 +0530

    Lecture 1 done

---
+++ lecture1/example1.js
@@ -0,0 +1,45 @@
+/*
+What is express ?
+==============================================
+  "Express or Express.js is a fast, unopinionated and minimalist web framework for Node.js" - official web site: Expressjs.com
+
+  In simple words we can say that Express.js is a web application framework for Node.js  that provides various features to make web application development fast and easy which otherwise takes more time using only Node.js.
+
+What is a framework ?
+===================================================
+  Frameworks are powerful tools that can make a developer’s job easier.
+
+  They provide a standardized set of design and development conventions that can be applied and modified for our web application.
+
+What does a framework do ?
+---------------------------------
+  If we look at any multiple web applications we can see that there are some common features like:
+      Routing
+      User authentication
+      Connect to databases 
+      Rendering pages etc
+  
+  Now , we can write all the above logic ourself but it will be simply waste of time . 
+  So to help a developer to eliminate the need to write a lot of repetitive code, there are lot of tools available which can do that for us. 
+  And they are called Framework.
+
+What express js can do ?
+=================================
+  Express Js helps a backend developer to perform following tasks:
+    Creating web server
+    Handling Routing
+    Developing API
+    Working With MiddleWare & Template Engines
+    Connect To The Databases
+
+Installng express
+===================================
+  We can install express in the following ways :
+    Installing express globally : 
+      We can install express with global scope in the following way:
+          npm install express -g
+
+    Installing express locally : 
+      We can install express with local scope in the following way:
+          npm installl express
+*/
