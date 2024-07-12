66fbaa4d7c9c10a82371a7e9cead66b2d4bea32c
pankajroy4 <pankajroy842002@gmail.com>
2024-07-13 02:09:18 +0530

    lecture 10 ongoing

---
+++ lecture10/apps/app2/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.set("view-engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+let users = [
+  { name: "Radha", email: "abc@gmail.com" },
+  { name: "Mohan", email: "def@gmail.com" },
+];
+
+app.use(express.static("public"));
+
+app.get("/", (req, resp) => {
+  resp.render("pages/addUser.ejs", { users: users });
+});
+
+app.post("/", (req, resp) => {
+  let name = req.body.uname;
+  let email = req.body.email;
+  if (name && email) users.push({ name: name, email: email });
+  resp.render("pages/showUsers.ejs", { users: users });
+});
+
+app.get("/users", (req, resp) => {
+  resp.render("pages/showUsers.ejs", { users: users });
+});
+
+const server = app.listen(3000, () => {
+  console.log(
+    `Server started and listening at http://localhost:${server.address().port}`
+  );
+});
---
+++ lecture10/example1.js
@@ -0,0 +1,22 @@
+/* 
+What is partial ?
+==========================
+  A partial is a reusable piece of HTML code that can be injected into other views. 
+
+  Using partials in our templates makes our code cleaner and maintains the DRY principle by allowing us to use the same piece of code across multiple templates without writing it more than once.
+
+How to use partials ?
+==================================
+To use partials in EJS, we take following steps:
+
+  1. Create a folder, preferably called, “partials” inside the “views” folder
+  2.Inside partials folder create the template file to be reused.
+
+  3.Finally, add the partial to our main files by using the keyword include inside a special tag <%- include(path) %> and passing the relative file path to the partial as an argument to the include keyword.
+
+  example:
+      <%- include("../partials/header.ejs") %>
+    See example folder app1
+  
+  Time 41 minutes
+*/
